7a7de2e7aa2076cd6d3b7ceea0d0248e681b9874
CrisCanonDev <criscanon21@gmail.com>
2024-05-10 13:37:36 +0300

    Initial commit

---
+++ src/test/java/com/ccdev/project5/Project5/Project5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccdev.project5.Project5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
